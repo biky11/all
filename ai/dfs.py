8fc256b0d7ab81e98a4af4ffb2ab6b91e55a67d0
biky69 <bikywuw@gmail.com>
2025-10-16 12:32:06 +0530

    dfs.py

--- ai/dfs.py
+++ ai/dfs.py
@@ -1,24 +1,30 @@
-# Graph representation (Romania map example)
-graph = {
-    'oradea': ['zerind', 'sibiu', 'arad'],
-    'zerind': ['arad', 'oradea'],
-    'arad': ['timisoara', 'sibiu', 'zerind'],
-    'timisoara': ['arad'],
-    'sibiu': ['oradea', 'arad', 'rimnicu vilcea', 'fagaras'],
-    'rimnicu vilcea': ['fagaras', 'sibiu', 'pitesti'],
-    'pitesti': ['rimnicu vilcea', 'bucharest'],
-    'fagaras': ['bucharest', 'sibiu', 'rimnicu vilcea'],
-    'bucharest': ['fagaras', 'pitesti']
-}
+import networkx as nx;
+import matplotlib.pyplot as plt;
 
-# Depth First Search (DFS)
-def dfs(node, visited):
-    if node not in visited:
-        print(node)
-        visited.add(node)
-        for neighbor in graph.get(node, []):
-            dfs(neighbor, visited)
+node=input("enter node");
+visit=set();
 
-# Start DFS
-visited = set()
-dfs('oradea', visited)
+graph={'kankavli':['devgad','oros'],
+       'devgad':['kankavli'],
+       'oros':['kankavli','malvan','kudal'],
+       'kudal':['malvan','oros','sawantwadi'],
+       'malvan':['oros','kudal'],
+       'sawantwadi':['kudal']}
+
+def dfs(node,visit,graph):
+    if node not in visit:
+        print(node);
+        visit.add(node);
+        for i in graph[node]:
+            dfs(i,visit,graph);
+
+dfs(node,visit,graph);
+
+G=nx.Graph();
+for node in graph:
+    for n in graph[node]:
+        G.add_edge(node,n);
+
+nx.draw(G,with_labels=True)
+plt.show();
+
